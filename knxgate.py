ad62507dc3c0b3ead555ec39fa4f33e6cd771f16
vvign <vincenzo.vignera@gmail.com>
2021-02-09 09:12:09 +0100

    Add files via upload
    Python Script to activate switch/scene/automation/light on KNX Vimar BUS.
    Tested Version Python 3.7.3

---
+++ knxgate.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import serial
+import sys
+import time
+
+
+line1 =  str(sys.argv[1])
+cmd =  str(sys.argv[2])
+actuator =  str(sys.argv[3])
+
+#DEBUG ARGUMENT
+#print "linea:" + line1
+#print "attuatore:" + actuator
+#print "comando:" + cmd
+
+ll = "@D" + line1
+scr = "@w" + cmd + actuator
+#print(""+ll)
+#print(""+scr)
+
+data1 = "@D" +line1
+res = bytes(data1, 'utf-8')
+
+print(res)
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+ser.write(res)
+ser.close()
+
+time.sleep(0.2)
+
+data2 = "@w" + cmd + actuator
+res2 = bytes(data2, 'utf-8')
+print(res2)
+ser2 = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+ser2.write(res2)
+ser2.close()
